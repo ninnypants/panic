799322d83ab49e20ec4d7d7388d7a9debf71ee40
ninnypants <tyrel@ninnypants.com>
2012-02-18 14:12:31 -0700

    Set up some basics

---
+++ config-sample.php
@@ -0,0 +1,15 @@
+<?php
+// database name
+define('DB_NAME', 'database_name');
+
+// database user
+define('DB_USER', 'database_user');
+
+// database password
+define('DB_PASSWORD', 'database_password');
+
+// database host
+define('DB_HOST', 'localhost');
+
+// debug?
+define('DEBUG', true);
